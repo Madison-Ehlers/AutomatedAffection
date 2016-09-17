13158a8fcdff0f701a01c393c37cbc40ea5335a0
Carter Scheve <Carter Scheve>
2016-09-17 15:41:18 -0500

    Settings Template Updated

--- app/src/main/java/com/example/mjehl/myapplication/SettingsActivity.java
+++ app/src/main/java/com/example/mjehl/myapplication/SettingsActivity.java
@@ -3,30 +3,34 @@ package com.example.mjehl.myapplication;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //import android.support.v7.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat;
+import android.util.SparseBooleanArray;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
+
+    public static boolean notificationToggle;
+
+    private static final int RESULT_SETTINGS = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-
-        String[] menuList = {"Send Notifications", "Calendar Sync", "Connect to Database"};
-        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuList);
-        /*ListView list = (ListView) findViewByID(R.id.list);
-
-        listView.setAdapter(adapter);*/
+        addPreferencesFromResource(R.xml.preferences);
     }
 
-    public void notifyTest(View view) {
+    private void notifyTest(View view) {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
         .setSmallIcon(R.drawable.ic_notification_custom)
         .setContentTitle("Test Notification")
@@ -39,6 +43,5 @@ public class SettingsActivity extends AppCompatActivity {
         NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         mBuilder.setAutoCancel(true);
         mNotifyMgr.notify(001, mBuilder.build());
-        //mNotifyMgr.cancel(001);
     }
 }
